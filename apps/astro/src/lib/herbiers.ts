1b44f49a7d1364bcc07cce2cc9088ad42d1e076c
Loïc Hamet <1453246+loic-h@users.noreply.github.com>
2026-09-01 12:08:10 +0200

    Oprimise herbiers with external symbols

--- apps/astro/src/lib/herbiers.ts
+++ apps/astro/src/lib/herbiers.ts
@@ -1,14 +1,4 @@
-import HerbierLeft1 from '@/assets/svgs/plants/herbier-left-1.svg'
-import HerbierLeft2 from '@/assets/svgs/plants/herbier-left-2.svg'
-import HerbierLeft3 from '@/assets/svgs/plants/herbier-left-3.svg'
-import HerbierLeft4 from '@/assets/svgs/plants/herbier-left-4.svg'
-import HerbierLeft5 from '@/assets/svgs/plants/herbier-left-5.svg'
-
-import HerbierCentre1 from '@/assets/svgs/plants/herbier-centre-1.svg'
-import HerbierCentre2 from '@/assets/svgs/plants/herbier-centre-2.svg'
-import HerbierCentre3 from '@/assets/svgs/plants/herbier-centre-3.svg'
-import HerbierCentre4 from '@/assets/svgs/plants/herbier-centre-4.svg'
-import HerbierCentre5 from '@/assets/svgs/plants/herbier-centre-5.svg'
+import Plant from '@/components/Plant.astro'
 
 import type { SvgComponent } from 'astro/types'
 import type { PageSlug } from '@monolithe/api/types'
@@ -27,27 +17,27 @@ type HerbierType = 'left' | 'centre'
 
 type Herbier = {
   slug: (typeof pagesPlantsOrder)[number]
-  plant: Plant
+  type: HerbierType
+  index: number
 }
 
 type Herbiers = Array<Herbier>
 
-const herbiers: Record<HerbierType, Array<SvgComponent>> = {
-  left: [HerbierLeft1, HerbierLeft2, HerbierLeft3, HerbierLeft4, HerbierLeft5],
-  centre: [HerbierCentre1, HerbierCentre2, HerbierCentre3, HerbierCentre4, HerbierCentre5],
-}
+// const herbiers: Record<HerbierType, Array<SvgComponent>> = {
+//   left: [HerbierLeft1, HerbierLeft2, HerbierLeft3, HerbierLeft4, HerbierLeft5],
+//   centre: [HerbierCentre1, HerbierCentre2, HerbierCentre3, HerbierCentre4, HerbierCentre5],
+// }
 
 export const getHerbier = (type: HerbierType): Herbiers => {
   return pagesPlantsOrder.map((slug, index) => {
     return {
       slug,
-      plant: herbiers[type][index],
+      type,
+      index,
     }
   })
 }
 
-export const getPlant = (slug: PageSlug, type: HerbierType = 'left') => {
-  const herbier = getHerbier(type)
-  const index = pagesPlantsOrder.indexOf(slug)
-  return herbier[index]
+export const getPlantIndex = (slug: PageSlug, type: HerbierType = 'left') => {
+  return pagesPlantsOrder.indexOf(slug)
 }
